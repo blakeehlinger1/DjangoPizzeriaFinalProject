ea3d58adcec95baf3c4eb8c18ae3a1e65a1bd06f
Blake Ehlinger <blake_ehlinger1@baylor.edu>
2022-05-03 21:05:19 -0500

    BE recent pro work

---
+++ DjangoShell.py
@@ -0,0 +1,22 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE","Pizzeria.settings")
+
+import django
+django.setup()
+
+from pizzas.models import Pizza
+
+pizzas = Pizza.objects.all()
+
+for p in pizzas:
+    print(p.id, ' ', p)
+
+p = Pizza.objects.get(id=1)
+
+toppings = p.topping_set.all()
+
+for t in toppings:
+    print(t)
+
+#print(p.text)
+#print(p.date_added)
